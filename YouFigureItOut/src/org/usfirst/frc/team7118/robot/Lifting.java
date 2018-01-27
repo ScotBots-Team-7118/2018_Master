a0998c72a0b40bc12da13bba7ba7a0e7dcbefe27
MusicShine <kg0979885@k12.shorelineschools.org>
2018-01-27 15:08:45 -0800

    Work in progress from saturday

--- YouFigureItOut/src/org/usfirst/frc/team7118/robot/Lifting.java
+++ YouFigureItOut/src/org/usfirst/frc/team7118/robot/Lifting.java
@@ -3,23 +3,64 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import edu.wpi.first.wpilibj.Encoder;
 public class Lifting {
-	Lift_Path lift_path;
+	LiftState lift_path;
 	Drive drive;
 	Sensors sensors;
 	TalonSRX talLift;
 	boolean ground = false;
 	boolean cwitch = false;
 	boolean scale = false;
+	boolean lastButtonS = false;
+	boolean lastButtonSc = false;
+	boolean lastButtonG = false;
+	LiftState currentState;
+	LiftState targetState;
+	
 	
 	public Lifting() {
 		talLift = new TalonSRX(4);
 		
 	}
-	public enum Lift_Path{
-		Scale,Switch,Ground
-		
+	public enum LiftState{
+		Scale,Switch,Ground,Moving
+	};
+	
+	private boolean getButtonState(LiftState whichOne)
+	{
+		if (whichOne == LiftState.Scale) {
+			return joyR.getRawButton(4);
+		}else if (whichOne == LiftState.Switch) {
+			return joyR.getRawButton(5);
+		}else if (whichOne == LiftState.Ground) {
+			return joyR.getRawButton(2);
+		}
 	}
+	
+	public void init() {
+		lastButtonS = getButtonState(LiftState.Scale);
+		lastButtonSc = getButtonState(LiftState.Switch);
+		lastButtonG = getButtonState(LiftState.Ground);
+	}
+	
 public void lift() {
+	
+	boolean currentButtonS = getButtonState(LiftState.Scale);
+	boolean currentButtonsc = getButtonState(LiftState.Switch);
+	if(currentButtonS != lastButtonS) {
+		
+		lastButtonS = currentButtonS;
+	}
+	
+	
+	
+	lastButtonSc = getButtonState(LiftState.Switch);
+	lastButtonG = getButtonState(LiftState.Ground);
+	if (joyR.getRawButton(2) == true && lastButtonG ==false){
+		;
+	}
+	
+
+	
 	switch(lift_path) {
 	case Scale:
 		if(joyR.getRawButton(4) && ground) {
@@ -40,7 +81,7 @@ public void lift() {
 	case Switch:
 		if(joyR.getRawButton(5) && scale) {
 			talLift.set(ControlMode.PercentOutput, -0.3);
-			if(sensors.encU.get())
+			if(sensors.encU.get()>= 10)
 		}
 		
 		
