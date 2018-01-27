f05283222fac7e6d7752a31053d37433e25eb270
MusicShine <kg0979885@k12.shorelineschools.org>
2018-01-27 14:05:28 -0800

    Skeleton Auto Code

--- YouFigureItOut/src/org/usfirst/frc/team7118/robot/Auto.java
+++ YouFigureItOut/src/org/usfirst/frc/team7118/robot/Auto.java
@@ -3,7 +3,24 @@ package org.usfirst.frc.team7118.robot;
 import org.usfirst.frc.team7118.robot.Scotstants;
 
 public class Auto {
-	public Auto() {
+	public Auto() { 
+		Auto_Path auto_path;
 		
 	}
+public enum Auto_Path{
+	//add steps for auto code here
+	
+}
+public void auto() {
+	switch(auto_path) {
+	case: //step1
+		//insert auto code here
+		break;
+		
+	case://step2
+		//insert auto code here
+		break;
+	
+	}
+}
 }
--- YouFigureItOut/src/org/usfirst/frc/team7118/robot/Lifting.java
+++ YouFigureItOut/src/org/usfirst/frc/team7118/robot/Lifting.java
@@ -1,5 +1,52 @@
 package org.usfirst.frc.team7118.robot;
-
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.Encoder;
 public class Lifting {
-
+	Lift_Path lift_path;
+	Drive drive;
+	Sensors sensors;
+	TalonSRX talLift;
+	boolean ground = false;
+	boolean cwitch = false;
+	boolean scale = false;
+	
+	public Lifting() {
+		talLift = new TalonSRX(4);
+		
+	}
+	public enum Lift_Path{
+		Scale,Switch,Ground
+		
+	}
+public void lift() {
+	switch(lift_path) {
+	case Scale:
+		if(joyR.getRawButton(4) && ground) {
+			talLift.set(ControlMode.PercentOutput,0.3);
+			if(sensors.encU.get() >= 20) {
+				talLift.set(ControlMode.PercentOutput,0);
+				scale = true;
+				}// when the arm gets to the number of rotations to the scale the motor and the arms will stop
+			}
+		if(joyR.getRawButton(4) && cwitch) {
+			talLift.set(ControlMode.PercentOutput,0.3);
+			if(sensors.encU.get() >= 10) {
+				talLift.set(ControlMode.PercentOutput,0);
+				scale = true;
+				}// when the arm gets to the number of rotations to the scale the motor and the arms will stop
+			}
+		break;
+	case Switch:
+		if(joyR.getRawButton(5) && scale) {
+			talLift.set(ControlMode.PercentOutput, -0.3);
+			if(sensors.encU.get())
+		}
+		
+		
+	}
+	
+	
+	
+	}
 }
